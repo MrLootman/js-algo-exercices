2276cb4736ff2a309a0f43a5e999e7e73193d65b
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2023-04-03 16:58:03 +0200

    💄 Rectification du fichier de test sur le quatrième algo

--- 4/algo.js
+++ 4/algo.js
@@ -1,8 +1,8 @@
 // Travail sur les instructions conditionnelles : 
 
-// La fonction doit retourner la phrase "J'ai plus de dix-huit ans" 
+// La fonction doit retourner la phrase "J'ai plus de 18 ans" 
 // si le paramètre age est supérieur à 18.
-// Sinon, la fonction retourne "J'ai moins de dix-huit ans".
+// Sinon, la fonction retourne "J'ai moins de 18 ans".
 
 function isMajor(age) {
   
